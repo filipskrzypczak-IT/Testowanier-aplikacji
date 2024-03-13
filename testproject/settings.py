042408ff44644b6592ea0dcd00ef3a8928dbd768
filipskrzypczak-IT <filipskrzypczak.it@gmail.com>
2024-03-13 16:06:31 +0100

    Dodawanie obrazko

--- testproject/settings.py
+++ testproject/settings.py
@@ -122,3 +122,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://d53378ada4d180990312e37b540bc6fb@o4506904108138496.ingest.us.sentry.io/4506904112201728",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
--- testproject/urls.py
+++ testproject/urls.py
@@ -16,7 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+def index(request):
+        return HttpResponse("Welcome to my Django project!")
 
 urlpatterns = [
+    path('', index),  # Handle the root URL
+    path('sentry-debug/', trigger_error),
     path('admin/', admin.site.urls),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
